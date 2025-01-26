e0b090976b0dc1ae04ad6d29d463edbf5d2c7fd2
Diya-Mukherjee2003 <diyamukherjee904@gmail.com>
2025-01-26 19:05:44 +0530

    Moved project files from Mongoos to Node_hotels

--- Node_hotels/server.js
+++ Node_hotels/server.js
@@ -11,9 +11,7 @@ app.use(bodyParser.json());
 app.get('/', (req, res)=> {
   res.send('Welcome to my hotel..how can I help you?')
 });
-// app.get('/chicken',(req,res)=>{
-//     res.send("Chicken is out of stock");
-// })
+
 
 app.post('/',async(req,res)=>{
   try{
@@ -25,12 +23,6 @@ app.post('/',async(req,res)=>{
     }
 
     const newPerson = new Person(data);
-  /*newPerson.name=data.name;
-    newPerson.age=data.age;
-    newPerson.mobile=data.mobile;   //Dont need to use
-    newPerson.email=data.email;
-    newPerson.address=data.address;
-    newPerson.name=data.name;*/ 
     const response=await newPerson.save();
     console.log('data saved');
     res.status(200).json(response);
@@ -40,16 +32,6 @@ app.post('/',async(req,res)=>{
     res.status(500).json({error:'Internal server error'});
   }
 
-  /*newPerson.save((error,savedPerson)=>{
-    if(error){
-      console.log("Error in saving person's data",error);
-      res.status(500).json({error:'Internal server error'})
-    }
-    else{                    //This is not used now
-      console.log("data saved successfully");
-      res.status(200).json(savedPerson);
-    }
-  })*/
 })
 app.get('/',async(req,res)=>{
   try{
